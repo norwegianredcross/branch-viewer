aac2e854743912f414f851eb35f662e43ce0df94
Terje Christensen <terchris@users.noreply.github.com>
2025-02-18 11:01:56 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/branch-viewer/',
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/main.tsx',
-      name: 'BranchViewer',
-      fileName: 'branch-viewer',
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
