179b0078ae7907a93b8ad145976822c78507b0fb
Terje Christensen <terchris@users.noreply.github.com>
2025-02-18 08:42:26 +0100

    moved testdata

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,4 +1,4 @@
-import organizationsData from '../secrets/organizations.json';
+import organizationsData from './testdata/organizations.json';
 import { BranchResponse } from './types/Branch';
 
 // Type assertion to ensure the data matches our BranchResponse type
